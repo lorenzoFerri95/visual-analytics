2621191b13af2b5cce6dc2b3b8e46f7be4aefffe
Lorenzo Ferri <lorenzoferri1995@gmail.com>
2021-10-15 22:05:44 +0200

    prova caricamento dati dei nodi

---
+++ src/components/Network.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Network",
+  data: function() {
+    return {
+      nodes: [],
+    };
+  },
+  mounted: function() {
+    fetch("./static/data/nodes.json")
+      .then((res) => res.json())
+      .then(function(data) {
+          console.log(data);
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
